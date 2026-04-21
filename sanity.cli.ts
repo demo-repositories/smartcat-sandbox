ec8ba79f4245094469f9a355df5bd50db81356ef
Rich Higgins <hello@iamrichhiggins.com>
2026-04-21 07:55:59 +0100

    added persistent studio hostname

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,7 +3,7 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '4b7y61cv',
-    dataset: 'production'
+    dataset: 'production',
   },
   deployment: {
     /**
@@ -11,5 +11,7 @@ export default defineCliConfig({
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
     autoUpdates: true,
-  }
+  },
+  // Tip: You can use an environment variable for studioHost if you want to deploy separate Studios for production, staging, testing etc.
+  studioHost: 'smartcat-sandbox',
 })
